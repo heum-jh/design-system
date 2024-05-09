29af4c70b86e484921425efeb5b0008c2e0adcf4
heum-jh <gihum@wavlie.io>
2024-05-09 19:33:19 +0900

    fix: remove

--- .rollup.cache/Users/ljiheum/Developer/design-system/dist/index.js
+++
@@ -1,8 +0,0 @@
-import "./styles/tailwind.css";
-export { Page } from "./components/Page";
-export { Header } from "./components/Header";
-export { Button } from "./components/Button";
-export { TaskList } from "./components/TaskList";
-export { Task, SkeletonTask } from "./components/Task";
-export { Input } from "./components/Input";
-//# sourceMappingURL=index.js.map
