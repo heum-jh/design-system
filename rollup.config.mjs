8ee0ca2e18fcfd9252bd561664a48e1ce9acd939
siyul-jh <gihum@naver.com>
2024-07-17 16:25:35 +0900

    fix: taillwind

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,12 +1,11 @@
 import commonjs from "@rollup/plugin-commonjs";
 import resolve from "@rollup/plugin-node-resolve";
 import typescript from "@rollup/plugin-typescript";
-import dts from "rollup-plugin-dts";
+import autoprefixer from "autoprefixer";
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import postcss from "rollup-plugin-postcss";
 import { terser } from "rollup-plugin-terser";
 import tailwindcss from "tailwindcss";
-import autoprefixer from "autoprefixer";
 
 import { createRequire } from "node:module";
 const requireFile = createRequire(import.meta.url);
@@ -14,38 +13,32 @@ const packageJson = requireFile("./package.json");
 
 const extensions = [".js", ".jsx", ".ts", ".tsx", ".css"];
 
-export default [
-  {
-    input: "./src/index.ts",
-    output: [
-      {
-        file: packageJson.main,
-        format: "cjs",
-        sourcemap: true,
-      },
-      {
-        file: packageJson.module,
-        format: "esm",
-        sourcemap: true,
-      },
-    ],
-    watch: {
-      include: "*",
-      exclude: "node_modules/**",
+export default {
+  input: "./src/index.ts",
+  output: [
+    {
+      file: packageJson.main,
+      format: "cjs",
+      sourcemap: true,
     },
-    plugins: [
-      peerDepsExternal(),
-      resolve({ extensions }),
-      commonjs({ include: "/node_modules/**" }),
-      typescript(),
-      postcss({
-        minimize: true,
-        modules: true,
-        extract: true,
-        plugins: [tailwindcss, autoprefixer],
-      }),
-      terser(),
-    ],
-    external: ["react", "react-dom"],
-  },
-];
+    {
+      file: packageJson.module,
+      format: "esm",
+      sourcemap: true,
+    },
+  ],
+  plugins: [
+    peerDepsExternal(),
+    resolve({ extensions }),
+    commonjs({ include: "/node_modules/**" }),
+    typescript(),
+    postcss({
+      minimize: true,
+      modules: true,
+      extract: true,
+      plugins: [tailwindcss, autoprefixer],
+    }),
+    terser(),
+  ],
+  external: ["react", "react-dom"],
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  prefix: "w-",
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {},
